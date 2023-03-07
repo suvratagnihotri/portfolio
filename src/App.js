52f569baaa3773cccf5b50e007c45e6411778a11
Suvrat Agnihotri <suvrat72000@gmail.com>
2023-03-07 08:54:02 +0530

    Reinitialised git remository

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import Portfolio from './components/portfolio/portfolio';
 
 function App() {
   return (
-    <Portfolio></Portfolio>
+    <div>
+      
+    </div>
   );
 }
 
